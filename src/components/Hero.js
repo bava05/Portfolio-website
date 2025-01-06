40c66df773a1cc35164be4bad618c946aaaae889
bava05 <149072637+bava05@users.noreply.github.com>
2025-01-06 20:51:23 +0530

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,13 +1,12 @@
 import HeroImg from '../assets/hero.png';
-import {AiOutlineGithub,AiOutlineLinkedin,AiOutlineMail} from "react-icons/ai";
+import {AiOutlineGithub,AiOutlineLinkedin} from "react-icons/ai";
 
 export default function Hero() {
     const config  = {
         subtitle: 'Im a Web Developer.',
         social: {
             github: 'https://github.com/bava05',
-            linkedin: 'https://www.linkedin.com/in/bavadhaarini-kumar-3010b126a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
-            gmail:'mail to:bavadhaarinikumar@gmail.com'
+            linkedin: 'https://www.linkedin.com/in/bavadhaarini-kumar-3010b126a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app'
         }
     }
 
@@ -20,7 +19,6 @@ export default function Hero() {
             <div className='flex py-10'>
                 <a href={config.social.github} className='pr-5 hover:text-white'><AiOutlineGithub size={40} /></a>
                 <a href={config.social.linkedin} className='pr-5 hover:text-white'><AiOutlineLinkedin size={40}/></a>
-                <a href={config.social.gmail} className='hover:text-white'><AiOutlineMail size={40}/></a>
             </div>
        </div>
        <img className='md:w-1/3' src={HeroImg} />
